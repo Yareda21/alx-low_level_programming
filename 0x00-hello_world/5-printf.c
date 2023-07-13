f1ef380033ac54dbdbdc5ba6dab26d68bfe1e240
Yareda21 <ykebede39@yahoo.com>
2023-07-13 05:43:15 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/* 
- * this is a comment 
- * for the printf 
- * function 
-*/
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
 /*this is a description of main*/
