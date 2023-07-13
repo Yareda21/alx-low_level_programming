c0d3903341cd75cefd04b6413c37c68b50c79eb4
Yareda21 <ykebede39@yahoo.com>
2023-07-13 05:36:24 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/* this is a comment 
+/* 
+ * this is a comment 
  * for the printf 
- * function */
+ * function 
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
