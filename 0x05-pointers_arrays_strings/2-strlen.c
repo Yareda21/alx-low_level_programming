9519129a209cd6921b862fcdbdacd4feb0cc857b
Yareda21 <ykebede39@yahoo.com>
2023-07-24 05:07:53 -0700

    sa

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,9 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * *s: The string to get the length of.
  *
- * Return: The length of @str.
+ * Return: The length of *s.
  */
 
 int _strlen(char *s)
