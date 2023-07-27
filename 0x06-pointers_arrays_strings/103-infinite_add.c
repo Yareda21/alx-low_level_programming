905155c6a89287a9096111237f509875f4d19c40
Yareda21 <ykebede39@yahoo.com>
2023-07-26 23:29:57 -0700

    mine

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -25,33 +25,6 @@ void rev_string(char *n)
 		*(n + i) = temp;
 	}
 }
-#include "main.h"
-
-/**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
- */
-
-void rev_string(char *n)
-{
-	int i = 0;
-	int j = 0;
-	char temp;
-
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
-
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
-}
 
 /**
  * infinite_add - add 2 numbers together
