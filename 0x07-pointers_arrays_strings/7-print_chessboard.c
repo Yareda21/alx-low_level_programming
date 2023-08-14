8184fe441380b482dbcadd831f303fbe91e67f42
Yareda21 <ykebede39@yahoo.com>
2023-08-14 01:54:06 -0700

    a

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,25 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_chessboard - Entry point
- * @a: array
- * Return: Always 0 (Success)
+ * print_chessboard - prints the chessboard
+ * @a: the board to print
  */
+
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; i < 8; i++)
+	i = j = 0;
+	while (i < 8)
 	{
-		for (j = 0; j < 8; j++)
+		j = 0;
+		while (j < 8)
 		{
 			_putchar(a[i][j]);
+			j++;
 		}
 		_putchar('\n');
+		i++;
 	}
-}
 
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is beggining of C programing language
+0x04-more_functions_nested_loops
